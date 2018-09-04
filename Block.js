d8033781562321de3390bcedb7f0907827c523a1
remmoze <remmoze@gmail.com>
2018-09-04 19:03:26 +0500

    blockjs

---
+++ Block.js
@@ -0,0 +1,34 @@
+class Block {
+	constructor(id, x, y) {
+		this.id = id
+		this.x = x;
+		this.y = y;
+		this.traits = [];
+		this.image = Game.getImageBlock(id);
+	}
+
+	onEnter(player) {
+		this.traits.forEach(trait => trait.onEnter(player));
+	}
+
+	isSolid() {
+		for (let i = 0; i < this.traits.length; i++) {
+			let solid = this.traits[i].isSolid();
+			if(solid === undefined) continue;
+			return solid;
+		}
+	}
+
+	addTrait(trait){
+		this.traits.push(trait);
+		this[trait.NAME] = trait;
+   	}
+
+   	update(){
+		this.traits.forEach(trait => trait.update()); //update Traits
+   	}
+
+   	draw(context) {
+   		context.drawImage(this.image, x * 24, y * 24);
+   	}
+}
